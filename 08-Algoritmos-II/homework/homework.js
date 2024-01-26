0725c9ea7115e65472b2b19129e3418184af227b
Vash <jpodesta.tkd@gmail.com>
2024-01-25 22:24:47 -0300

    m1 algoritmos 2 terminado

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -27,12 +27,34 @@ function quickSort(array) {
   return arrayOrdenado
 }
 
+function merge(izq, der) {
+  let i = 0
+  let j = 0
+  let result = []
+  while (i < izq.length && j < der.length) {
+    if (izq[i] < der[j]) {
+      result.push(izq[i])
+      i++
+    } else {
+      result.push(der[j])
+      j++
+    }
+  }
+  return result.concat(izq.slice(i)).concat(der.slice(j))
+}
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length === 1) {
+    return array
+  }
+  let mid = Math.floor(array.length / 2)
+  let izq = array.slice(0, mid)
+  let der = array.slice(mid)
 
+  return merge(mergeSort(izq), mergeSort(der))
   //okey probemos ahora
 }
 
